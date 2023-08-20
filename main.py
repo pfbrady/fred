dfe6bf1d043678fccf1cfdf0625b1de070a60c81
pfbrady <pbrady@udel.edu>
2023-08-20 18:33:14 -0400

    W2W + Bot templates

---
+++ main.py
@@ -0,0 +1,21 @@
+import settings
+import discord
+import requests
+from discord.ext import commands
+
+def run():
+    intents = discord.Intents.default()
+    req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
+    req_json = req.json()
+    print(req_json['EmployeeList'][0])
+
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(bot.user)
+
+    bot.run(settings.DISCORD_TOKEN)
+
+if __name__ == "__main__":
+    run()
---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+W2W_TOKEN = os.getenv('W2W_TOKEN')
