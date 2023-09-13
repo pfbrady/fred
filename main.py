ca578864e3860d9ebb862dd0faf284731b86c213
isaoudcareer <isaoudcareer@gmail.com>
2023-09-13 11:36:41 -0400

    IS-1.1

--- main.py
+++ main.py
@@ -2,30 +2,14 @@ import settings
 import discord
 import requests
 from discord.ext import commands
-import datetime
-import time
 
 
 def run():
-    while True:
-        #intents = discord.Intents.default()
+        intents = discord.Intents.default()
+        req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
+        req_json = req.json()
 
-        #req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
-        #req_json = req.json()
-
-        today = datetime.date.today()
-        end_date = today + datetime.timedelta(days=14) #2 weeks from now.
-
-        today = today.strftime("%m/%d/%Y")
-        end_date = end_date.strftime("%m/%d/%Y")
-
-        req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/AssignedShiftList?start_date=' + 
-                        today + '&end_date=' + end_date + '&key=' + settings.W2W_TOKEN)
-                            
-        req_json = req.json() 
-        print (req_json['AssignedShiftList'])
-
-        '''''
+        print(req_json["EmployeeList"][0])
         bot = commands.Bot(command_prefix="!", intents=intents)
 
         @bot.event
@@ -33,12 +17,7 @@ def run():
             print(bot.user)
 
         bot.run(settings.DISCORD_TOKEN)
-        '''''
 
-        # 5 seconds sleep
-        time.sleep(5)
-        print("---------------------------------------------------------------------------------")
-        print("----------------------------Sleeping for 5 seconds...----------------------------")
 
 if __name__ == "__main__":
     run()
