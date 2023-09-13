e2a4d666da4c10a995fe87e1d6bf9d434633e76f
isaoudcareer <isaoudcareer@gmail.com>
2023-09-13 11:01:47 -0400

    IS-1.0

--- main.py
+++ main.py
@@ -2,26 +2,43 @@ import settings
 import discord
 import requests
 from discord.ext import commands
+import datetime
+import time
+
 
 def run():
-    intents = discord.Intents.default()
-    req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
-    req_json = req.json()
-    print(req_json['EmployeeList'][0])
-    req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/AssignedShiftList?start_date=' + '09/09/2023' + '&end_date=' + '09/10/2023' +' &key=' + settings.W2W_TOKEN)
-    req_json = req.json()
-    print(req_json['AssignedShiftList'][-1])
+    while True:
+        #intents = discord.Intents.default()
 
-    bot = commands.Bot(command_prefix="!", intents=intents)
+        #req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
+        #req_json = req.json()
 
-    # stuff
-    
-    # hello
-    @bot.event
-    async def on_ready():
-        print(bot.user)
+        today = datetime.date.today()
+        end_date = today + datetime.timedelta(days=14) #2 weeks from now.
 
-    bot.run(settings.DISCORD_TOKEN)
+        today = today.strftime("%m/%d/%Y")
+        end_date = end_date.strftime("%m/%d/%Y")
+
+        req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/AssignedShiftList?start_date=' + 
+                        today + '&end_date=' + end_date + '&key=' + settings.W2W_TOKEN)
+                            
+        req_json = req.json() 
+        print (req_json['AssignedShiftList'])
+
+        '''''
+        bot = commands.Bot(command_prefix="!", intents=intents)
+
+        @bot.event
+        async def on_ready():
+            print(bot.user)
+
+        bot.run(settings.DISCORD_TOKEN)
+        '''''
+
+        # 5 seconds sleep
+        time.sleep(5)
+        print("---------------------------------------------------------------------------------")
+        print("----------------------------Sleeping for 5 seconds...----------------------------")
 
 if __name__ == "__main__":
     run()
