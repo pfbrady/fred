4af1a403e767865ea6098f4cc97584b2edda8ddd
pfbrady <pbrady@udel.edu>
2023-09-13 09:39:52 -0400

    Added new request

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def run():
     req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
     req_json = req.json()
     print(req_json['EmployeeList'][0])
+    req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/AssignedShiftList?start_date=' + '09/09/2023' + '&end_date=' + '09/10/2023' +' &key=' + settings.W2W_TOKEN)
+    req_json = req.json()
+    print(req_json['AssignedShiftList'][-1])
 
     bot = commands.Bot(command_prefix="!", intents=intents)
 
