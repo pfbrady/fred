53451c74ce97ced66b39d8bb1cc5fb3f7d159b49
isaoudcareer <isaoudcareer@gmail.com>
2023-09-20 13:30:58 -0400

    9/20 updates

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ from discord.ext import commands
 
 
 def run():
-        intents = discord.Intents.default()
-        req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
-        req_json = req.json()
+        #req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
+        #req_json = req.json()
+        #print(req_json["EmployeeList"][0])
 
-        print(req_json["EmployeeList"][0])
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+
         bot = commands.Bot(command_prefix="!", intents=intents)
 
         @bot.event
         async def on_ready():
             print(bot.user)
+            for guild in bot.guilds:
+                for channel in guild.text_channels:
+                    if channel.name == 'test':
+                        await channel.send("Yo, I'm your future bot. Call me Fred :)")
 
-        bot.run(settings.DISCORD_TOKEN)
+
+        #bot.run(settings.DISCORD_TOKEN)
 
 
 if __name__ == "__main__":
