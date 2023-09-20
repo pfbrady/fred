6fcb828a0e6d238ff9ed11e512f0d69273229af6
Ilyas <77984410+isaoudcareer@users.noreply.github.com>
2023-09-20 13:32:37 -0400

    Is 1.0 (#8)
    * IS-1.0
    
    * IS-1.1
    
    * adding opne_shifts
    
    * changes
    
    * 9/20 updates

--- main.py
+++ main.py
@@ -3,25 +3,29 @@ import discord
 import requests
 from discord.ext import commands
 
+
 def run():
-    intents = discord.Intents.default()
-    req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
-    req_json = req.json()
-    print(req_json['EmployeeList'][0])
-    req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/AssignedShiftList?start_date=' + '09/09/2023' + '&end_date=' + '09/10/2023' +' &key=' + settings.W2W_TOKEN)
-    req_json = req.json()
-    print(req_json['AssignedShiftList'][-1])
+        #req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
+        #req_json = req.json()
+        #print(req_json["EmployeeList"][0])
 
-    bot = commands.Bot(command_prefix="!", intents=intents)
+        intents = discord.Intents.default()
+        intents.message_content = True
 
-    # stuff
-    
-    # hello
-    @bot.event
-    async def on_ready():
-        print(bot.user)
 
-    bot.run(settings.DISCORD_TOKEN)
+        bot = commands.Bot(command_prefix="!", intents=intents)
+
+        @bot.event
+        async def on_ready():
+            print(bot.user)
+            for guild in bot.guilds:
+                for channel in guild.text_channels:
+                    if channel.name == 'test':
+                        await channel.send("Yo, I'm your future bot. Call me Fred :)")
+
+
+        #bot.run(settings.DISCORD_TOKEN)
+
 
 if __name__ == "__main__":
     run()
