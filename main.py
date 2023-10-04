4f46d58effb391afd188c4e33d563c9d5cd780a7
isaoudcareer <isaoudcareer@gmail.com>
2023-10-04 12:56:27 -0400

    temp

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import settings
 import discord
-import requests
 from discord.ext import commands
 import get_open_shifts as gos
-import pytz, datetime
+import pytz
+import datetime
+import asyncio
 
-# Set the timezone to Eastern Standard Time (EST)
 est = pytz.timezone('US/Eastern')
-# Get the current time in EST
 current_time = datetime.datetime.now(est)
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 
 async def send_unassigned_shifts():
@@ -20,8 +19,8 @@ async def send_unassigned_shifts():
     shifts_as_int = [int(shift) for shift in shifts_tuple]
     lifeguard_unassigned_shifts, aquatic_lead_unassigned_shifts, swim_instr_unassigned_shifts, swam_unassigned_shifts = shifts_as_int
         
-    if current_time.hour == 13 and current_time.minute == 41:
-        for guild in bot.guilds:
+    if current_time.hour == 15 and current_time.minute == 0:
+        for guild in client.guilds:
             for channel in guild.text_channels:
                 if channel.name == 'test' and lifeguard_unassigned_shifts != 0:
                     await channel.send(f"Hi, there are ({lifeguard_unassigned_shifts}) unassigned Lifeguard shifts tomorrow.")
@@ -33,23 +32,23 @@ async def send_unassigned_shifts():
                     await channel.send(f"Hi, there are ({swam_unassigned_shifts}) unassigned SWAM  shifts tomorrow.")
 
 
-def run():
-    @bot.event
-    async def on_ready():
-        print(bot.user)
-        await send_unassigned_shifts()
+class Fred:
+    def __init__(self, client):
+        self.client = client
 
-    bot.run(settings.DISCORD_TOKEN)
+    async def tasks(self):
+        unassigned_shifts = await send_unassigned_shifts()
+        return unassigned_shifts
 
-
+    async def on_ready(self):
+        print(f'Logged in as {self.client.user} (ID: {self.client.user.id})')
+        print('------')
+        while True:
+            await self.tasks()
+            await asyncio.sleep(3600)
 
 
 if __name__ == "__main__":
-    run()
-
-
-'''
-req = requests.get('https://www3.whentowork.com/cgi-bin/w2wC4.dll/api/EmployeeList?key=' + settings.W2W_TOKEN)
-req_json = req.json()
-print(req_json["EmployeeList"][0])
-'''
+    bot_instance = Fred(client)
+    client.loop.create_task(bot_instance.on_ready())
+    client.run(settings.DISCORD_TOKEN)
