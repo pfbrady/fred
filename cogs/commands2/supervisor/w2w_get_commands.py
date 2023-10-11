0b0b12ce0ec12b6179566ceaf560e4ee5821dc2c
pfbrady <50747698+pfbrady@users.noreply.github.com>
2023-10-11 10:32:41 -0400

    Pb features (#16)
    * Added new request
    
    * hello
    
    * fixed indentation
    
    * fixed naming
    
    * change
    
    * fred
    
    * fred
    
    * Ignore .db files
    
    * Database updates
    
    * test
    
    * added unassinged shifts to cogs
    
    * cleaned up database code
    
    * database final touches
    
    * sorry jayden
    
    * g

---
+++ cogs/commands2/supervisor/w2w_get_commands.py
@@ -0,0 +1,28 @@
+import datetime
+import discord
+from discord.ext import commands, tasks
+from w2w import get_assigned_shifts
+
+class W2W_Get_Commands(commands.Cog):
+
+    def __init__(self, Fred):
+        self.Fred = Fred
+    
+    @commands.command()
+    async def test(self, ctx, *, member:discord.Member, interaction:discord.Interaction):
+        await interaction.response.send_message(f'{member.display_name} joined on {member.joined_at.date()}')
+
+    @commands.command()
+    async def test2(self, ctx, interactions:discord.Integration):
+        today = datetime.date.today()
+        tomorrow = today + datetime.timedelta(days=1) #next day.
+        tomorrow = tomorrow.strftime("%m/%d/%Y")
+        tomorrow_staff = get_assigned_shifts(tomorrow)
+        await ctx.send(f"Here is a list of all of the employees working tomorrow:")
+
+    # @commands.command()
+    # async def everyone_tomorrow(self, ctx):
+    #     await ctx.send()
+
+async def setup(Fred):
+    await Fred.add_cog(W2W_Get_Commands(Fred))
---
+++ w2w.py
@@ -0,0 +1,10 @@
+import settings
+import requests
+import datetime
+import time
+
+def get_assigned_shifts(date, roles=None):
+    pass
+
+def get_assigned_shifts(start_date, end_date, roles=None):
+    pass
