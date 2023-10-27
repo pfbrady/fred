4d236d4c490a0d02ded37c6cfca026972843013d
pfbrady <pbrady@udel.edu>
2023-10-27 18:22:38 -0400

    chsnges

--- cogs/commands2/supervisor/w2w_get_commands.py
+++ cogs/commands2/supervisor/w2w_get_commands.py
@@ -3,26 +3,23 @@ import discord
 from discord.ext import commands, tasks
 from w2w import get_assigned_shifts
 
-class W2W_Get_Commands(commands.Cog):
+class W2W_Get_Commands(discord.app_commands.Group):
 
-    def __init__(self, Fred):
-        self.Fred = Fred
-    
-    @commands.command()
-    async def test(self, ctx, *, member:discord.Member, interaction:discord.Interaction):
-        await interaction.response.send_message(f'{member.display_name} joined on {member.joined_at.date()}')
+    @discord.app_commands.command()
+    async def test(self, interaction:discord.Interaction):
+        await interaction.response.send_message(f'hrloo')
 
-    @commands.command()
-    async def test2(self, ctx, interactions:discord.Integration):
+    @discord.app_commands.command(description="Test2")
+    async def test2(self, interaction:discord.Integration):
         today = datetime.date.today()
         tomorrow = today + datetime.timedelta(days=1) #next day.
         tomorrow = tomorrow.strftime("%m/%d/%Y")
         #tomorrow_staff = get_assigned_shifts(tomorrow)
-        await ctx.send(f"Here is a list of all of the employees working tomorrow:")
+        await interaction.response.sendmessage(f"Here is a list of all of the employees working tomorrow:")
 
     # @commands.command()
     # async def everyone_tomorrow(self, ctx):
     #     await ctx.send()
 
 async def setup(Fred):
-    await Fred.add_cog(W2W_Get_Commands(Fred))
+    await Fred.tree.add_command(W2W_Get_Commands(name="w2w-get-commands"))
