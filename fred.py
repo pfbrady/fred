593e8304bb28a40f247b21dbdb808a8ca6dbea18
pfbrady <pbrady@udel.edu>
2023-10-30 20:47:42 -0400

    wd

--- fred.py
+++ fred.py
@@ -19,6 +19,7 @@ class Fred(Bot):
 
     async def on_ready(self):
         self.database.init_discord_users(self.get_all_members())
+        
         await cogs.tasks2.fred_tasks.setup(self)
         await self.load_extension("cogs.commands2.supervisor.w2w_get_commands")
         self.tree.copy_global_to(guild=self.guilds[0])
