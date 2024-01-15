4d2263497140b7d5132b9cc058737a165aa788c0
pfbrady <50747698+pfbrady@users.noreply.github.com>
2024-01-14 22:39:38 -0500

    test updates

--- tests/test_branch.py
+++ tests/test_branch.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from whentowork import Employee
 from fred.ymca import YMCA
 from settings import SETTINGS_DICT
 
@@ -7,6 +8,10 @@ class BranchTestCase(TestCase):
     def setUp(self):
         ymca = YMCA('YMCA of Delaware')
         self.branch_western = ymca.branches['007']
-         
+        
     def test_branch_name(self):
         self.assertEqual('Western', self.branch_western.name)
+
+    def test_get_w2w_employee_by_id(self):
+        test_emp = self.branch_western.get_w2w_employee_by_id(564685546)
+        self.assertIsInstance(test_emp, Employee)
