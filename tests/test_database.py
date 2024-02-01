ffe50425133213eae5a221939ac94ebfb8c498fc
pfbrady <pbrady@udel.edu>
2024-01-31 20:13:42 -0500

    testing the database connection

---
+++ tests/test_database.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from fred import YMCA
+
+class YMCADatabaseCase(TestCase):
+    def setUp(self):
+        ymca = YMCA('YMCA of Delaware')
+        self.database = ymca.database
+
+         
+    def test_database_connection(self):
+        self.assertEqual(True, bool(self.database.connection))
